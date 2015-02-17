482ef34ce935921dae0013715b31eb95b25d1ee3
GAO8A <rikgao@icloud.com>
2015-02-17 00:03:08 -0500

    added leaflet dependencies

--- config/env/production.js
+++ config/env/production.js
@@ -7,6 +7,7 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.min.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+				'public/lib/leaflet/dist/leaflet.css'
 			],
 			js: [
 				'public/lib/angular/angular.min.js',
@@ -17,7 +18,9 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+				'public/lib/leaflet/dist/leaflet.js',
+				'public/lib/angular-leaflet-directive/dist/angular-leaflet-directive.min.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
